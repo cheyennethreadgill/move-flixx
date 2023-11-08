7bf2d898637878f4aa6d9c9bed10443e00189c65
cethread <threadgillcheyenne@gmail.com>
2023-11-08 15:36:41 -0500

    r

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,24 @@
 const highRatingContainer = document.querySelector('.high-rating');
 const popularContainer = document.querySelector('.popular');
+const aZ = document.querySelector('.a-z');
 
 function Main(movies) {
+  // a-z
+
+  const aZArray = movies
+    .filter((movie) => {
+      if (movie.rating > 0) {
+        return movie;
+      }
+    })
+    .slice(0, 5);
+
   const highlyRatedMovies = movies.filter((movie) => {
     if (movie.rating >= 7.6) {
       return movie;
     }
   });
+
   const popularMovies = movies
     .filter((movie) => {
       if (movie.popularity >= 1000) {
@@ -14,6 +26,25 @@ function Main(movies) {
       }
     })
     .slice(0, 5);
+
+  const aZContent = aZArray
+    .map((movie) => {
+      const {img, rating, title} = movie;
+      return `
+      <div class="movie">
+      
+        <div class="movie-img-container">
+            <img
+            src=${img}
+            alt="Five Nights" /> 
+        </div>
+       
+        <h3>${title}</h3>
+        <span class="rating">${rating}</span>
+    </div>
+      `;
+    })
+    .join('');
 
   const highlyRatedMovieContent = highlyRatedMovies
     .map((movie) => {
@@ -50,10 +81,11 @@ function Main(movies) {
     </div>
       `;
     })
-        .join( '' );
-    
+    .join('');
+
   highRatingContainer.innerHTML = highlyRatedMovieContent;
   popularContainer.innerHTML = popularMoviesContent;
+  aZ.innerHTML = aZContent;
 }
 
 // Dont need, already rendering in the async function
