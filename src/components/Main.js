995f34c2adb6f122fc783650e3be35eedf27ac26
cethread <threadgillcheyenne@gmail.com>
2023-11-08 12:02:09 -0500

    initial commit

---
+++ src/components/Main.js
@@ -0,0 +1,31 @@
+const genre = document.querySelector('.genre');
+
+function Main(movies) {
+  const highlyRatedMovies = movies.filter((movie) => {
+    if (movie.rating >= 7.6) {
+      return movie;
+    }
+  });
+
+  const movieContent = highlyRatedMovies
+    .map((movie) => {
+      const {img, rating, title} = movie;
+      return `
+      <div class="movie">
+        <img
+            class="movie-img"
+            src=${img}
+            alt="Five Nights" />
+        <h2>${title}</h2>
+        <span class="rating">${rating}</span>
+    </div>
+      `;
+    })
+    .join('');
+  genre.innerHTML = movieContent;
+}
+
+// Dont need, already rendering in the async function
+//   Main();
+
+export default Main;
---
+++ src/script.js
@@ -0,0 +1,33 @@
+import Main from './components/Main.js';
+
+const MoviesList = async function () {
+  const API_URL =
+    ' https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1';
+  const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+  const resp = await fetch(API_URL);
+  const data = await resp.json();
+  const moviesGrab = await data.results;
+
+  // returning movie array
+  const movieList = moviesGrab.map((movie) => {
+    const movieData = {
+      title: movie.title,
+      rating: movie.vote_average,
+      ratingCount: movie.vote_count,
+      description: movie.overview,
+      img: IMG_PATH + movie.poster_path,
+      backgroundImg: IMG_PATH + movie.backdrop_path,
+      genre: movie.genre_ids,
+    };
+    return movieData;
+  });
+
+  console.log(moviesGrab);
+
+  // passing movie array to main component
+  Main(movieList);
+};
+
+MoviesList();
+
+export default MoviesList;
