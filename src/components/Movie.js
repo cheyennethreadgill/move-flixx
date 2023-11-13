7033f9010f416248e0a4943349e40c5b3f335838
cethread <threadgillcheyenne@gmail.com>
2023-11-13 12:34:16 -0500

    r

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,6 +1,21 @@
 function Movie(movie) {
   const { img, rating, title, popularity } = movie;
 
+  // Add rating colors
+  const ratingTags = document.querySelectorAll(".movie-text-rating");
+
+  ratingTags.forEach((tag) => {
+    if (tag.innerHTML >= 7) {
+      tag.classList.add("green");
+    }
+    if ((tag.innerHTML >= 5.4) & (tag.innerHTML <= 7)) {
+      tag.classList.add("orange");
+    }
+    if ((tag.innerHTML >= 0) & (tag.innerHTML <= 5.3)) {
+      tag.classList.add("red");
+    }
+  });
+
   return `
       <button class="movie" value=${popularity} value=${popularity}>
         <div class="movie-img-container">
---
+++ src/components/SingleMoviePage.js
@@ -0,0 +1,161 @@
+function SingleMoviePage(movies, movieSection, main, header) {
+  const movie = document.querySelectorAll(".movie");
+
+  // SHOW SINGLE MOVIE PAGE
+  movie.forEach((item) => {
+    item.addEventListener(
+      "click",
+      function showMovie() {
+        const itemValue = item.value;
+
+        let foundMovie = movies.filter((movie) => {
+          const { popularity } = movie;
+
+          if (popularity == itemValue) {
+            return movie;
+          }
+        });
+        let movie = foundMovie[0];
+        const {
+          backgroundImg,
+          img,
+          title,
+          description,
+          ratingCount,
+          rating,
+          popularity,
+        } = movie;
+
+        const movieContentSection = document.createElement("section");
+        const movieContent = `
+      <section class="single-movie">
+        <button class="btn-navigation">
+          <i
+            class="fa-solid fa-chevron-left fa-xl"
+            style="color: #8190eb"
+          ></i>
+        </button>
+
+        <div class="single-movie-img">
+          <img
+            src=${backgroundImg}
+            alt=${title}
+          />
+        </div>
+
+        <div class="single-movie-synopsis">
+          <div class="wrapper">
+            <section class="single-movie-content">
+              <div class="row">
+                <div class="col-2">
+                  <div class="img-container">
+                    <img
+                      src=${img}
+                      alt=${title}
+                    />
+                  </div>
+                </div>
+                <div class="col-10">
+                  <p class="single-movie-text-ratingcount">40,325</p>
+                  <h1>${title}</h1>
+                  <p class="single-movie-runtime">1 hr 35 min</p>
+                </div>
+              </div>
+
+              <p class="single-movie-description">
+                ${description}
+              </p>
+
+              <h2>Cast</h2>
+            </section>
+            <section class="single-movie-cast">
+              <div class="container">
+                <div class="single-movie-cast-div">
+                  <div class="single-movie-cast-div-member">
+                    <span class="placeholder"></span>
+                    <p class="single-movie-cast-div-member-name">Lorem Ipsum</p>
+                  </div>
+                </div>
+                <div class="single-movie-cast-div">
+                  <div class="single-movie-cast-div-member">
+                    <span class="placeholder"></span>
+                    <p class="single-movie-cast-div-member-name">Lorem Ipsum</p>
+                  </div>
+                </div>
+                <div class="single-movie-cast-div">
+                  <div class="single-movie-cast-div-member">
+                    <span class="placeholder"></span>
+                    <p class="single-movie-cast-div-member-name">Lorem Ipsum</p>
+                  </div>
+                </div>
+                <div class="single-movie-cast-div">
+                  <div class="single-movie-cast-div-member">
+                    <span class="placeholder"></span>
+                    <p class="single-movie-cast-div-member-name">Lorem Ipsum</p>
+                  </div>
+                </div>
+                <div class="single-movie-cast-div">
+                  <div class="single-movie-cast-div-member">
+                    <span class="placeholder"></span>
+                    <p class="single-movie-cast-div-member-name">Lorem Ipsum</p>
+                  </div>
+                </div>
+                <div class="single-movie-cast-div">
+                  <div class="single-movie-cast-div-member">
+                    <span class="placeholder"></span>
+                    <p class="single-movie-cast-div-member-name">Lorem Ipsum</p>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </div>
+        </div>
+        <div class="container">
+          <button class="btn-primary">Buy Movie</button>
+        </div>
+      </section>
+    `;
+        movieContentSection.innerHTML = movieContent;
+
+        main.appendChild(movieContentSection);
+
+        document.querySelectorAll(".movie-section").forEach((section) => {
+          section.classList.add("hide");
+          header.classList.add("hide");
+
+          if (section.classList.contains("show")) {
+            header.classList.add("hide");
+            header.classList.remove("show");
+
+            section.classList.add("hide");
+            section.classList.remove("show");
+          }
+        });
+
+        // GO BACK HOME
+        const backButtons = document.querySelectorAll(".btn-navigation");
+
+        backButtons.forEach((button) => {
+          button.addEventListener(
+            "click",
+            () => {
+              console.log("work");
+              document.querySelectorAll(".movie-section").forEach((section) => {
+                section.classList.remove("hide");
+                section.classList.add("show");
+
+                header.classList.remove("hide");
+                header.classList.add("show");
+              });
+              main.removeChild(movieContentSection);
+            },
+            false
+          );
+        });
+      },
+      false
+    );
+  });
+}
+
+export default SingleMoviePage;
