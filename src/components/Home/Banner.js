45a9eba2d63cf3722ee6cb21c83387827bb3c0fa
cethread <threadgillcheyenne@gmail.com>
2023-11-17 15:00:05 -0500

    refractor

---
+++ src/components/Home/Banner.js
@@ -0,0 +1,38 @@
+function Banner(header, movies) {
+  const banner = document.querySelector(".banner");
+
+  // return most popular movie
+  //   const mostPopularMovie = movies.filter((movie) => {
+  //     const { popularity } = movie;
+
+  //     if (popularity > 2000) {
+  //       return movie;
+  //     }
+  //   });
+  //   var mostPopularMovieData = mostPopularMovie[0];
+
+  // return randowm movie
+  const getRandomMovie = Math.floor(Math.random() * movies.length);
+  const randomMovie = movies[getRandomMovie];
+
+  const { backgroundImg, description, title } = randomMovie;
+  // parse banner content with random movie data
+  const bannerContent = `
+    <div class="banner_img">
+      <img
+        src=${backgroundImg}
+        alt=${title}
+      />
+      <div class="banner_body">
+        <h1 class="banner_heading">${title}</h1>
+        <h3 class="banner_text">
+          ${description}
+        </h3>
+      </div>
+    </div>
+  `;
+  // append banner content
+  banner.innerHTML = bannerContent;
+}
+
+export default Banner;
